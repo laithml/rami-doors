ec1c951d0bfa322d2aafe57c744dfdc5d74d39b6
mohammad Ghosheh <mouhammed.mx.4321@gmail.com>
2023-06-06 00:21:41 +0300

    bottom nav icons

--- navigation/bottomNav.js
+++ navigation/bottomNav.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {Text} from "react-native-paper";
-import FormScreen from "../screens/addForm";
+import { Ionicons } from '@expo/vector-icons';
+import FormScreen from '../screens/addForm';
 import HomeScreen from '../screens/Home';
+
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
     return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ color, size }) => {
+                    let iconName;
 
-        <Tab.Navigator>
-            <Tab.Screen name="Form"  component={FormScreen}/>
+                    if (route.name === 'Form') {
+                        iconName = 'document-outline';
+                    } else if (route.name === 'Home') {
+                        iconName = 'home-outline';
+                    } else if (route.name === 'Admin') {
+                        iconName = 'person-outline';
+                    }
+
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+        >
+            <Tab.Screen name="Form" component={FormScreen} />
             <Tab.Screen name="Home" component={HomeScreen} />
             <Tab.Screen name="Admin" component={FormScreen} />
-
         </Tab.Navigator>
     );
 };
 
-export default BottomTabNavigator
-
+export default BottomTabNavigator;
